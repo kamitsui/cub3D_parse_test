cb6ead90193d3d58de707218ac0c4d07a3bcaf2c
Yoshinari Kusano <ykusano@c1r16s8.42tokyo.jp>
2023-11-28 20:58:44 +0900

    cub3D test

---
+++ cub3D_parseTest/parse.sh
@@ -0,0 +1,76 @@
+./cub3D ./MAP/error/COLOR_ER1.cub
+echo
+./cub3D ./MAP/error/COLOR_ER2.cub
+echo
+./cub3D ./MAP/error/COLOR_ER3.cub
+echo
+./cub3D ./MAP/error/COLOR_ER4.cub
+echo
+./cub3D ./MAP/error/COLOR_ER5.cub
+echo
+./cub3D ./MAP/error/C_OVER.cub
+echo
+./cub3D ./MAP/error/C_less.cub
+echo
+./cub3D ./MAP/error/EA_OVER.cub
+echo
+./cub3D ./MAP/error/EA_less.cub
+echo
+./cub3D ./MAP/error/EXTRA_ELEMENTS.cub
+echo
+./cub3D ./MAP/error/EXTRA_ELEMENTS2.cub
+echo
+./cub3D ./MAP/error/EXTRA_ELEMENTS3.cub
+echo
+./cub3D ./MAP/error/EXTRA_ELEMENTS4 copy.cub
+echo
+./cub3D ./MAP/error/F_OVER.cub
+echo
+./cub3D ./MAP/error/F_less.cub
+echo
+./cub3D ./MAP/error/MAP_ELEMENT1.cub
+echo
+./cub3D ./MAP/error/MAP_ELEMENT2.cub
+echo
+./cub3D ./MAP/error/MAP_ELEMENT3.cub
+echo
+./cub3D ./MAP/error/MAP_ELEMENT4.cub
+echo
+./cub3D ./MAP/error/NO_OVER.cub
+echo
+./cub3D ./MAP/error/NO_PATH.cub
+echo
+./cub3D ./MAP/error/NO_SURROUNDED_WALL.cub
+echo
+./cub3D ./MAP/error/NO_SURROUNDED_WALL10.cub
+echo
+./cub3D ./MAP/error/NO_SURROUNDED_WALL2.cub
+echo
+./cub3D ./MAP/error/NO_SURROUNDED_WALL3.cub
+echo
+./cub3D ./MAP/error/NO_SURROUNDED_WALL4.cub
+echo
+./cub3D ./MAP/error/NO_SURROUNDED_WALL5.cub
+echo
+./cub3D ./MAP/error/NO_SURROUNDED_WALL6.cub
+echo
+./cub3D ./MAP/error/NO_SURROUNDED_WALL7.cub
+echo
+./cub3D ./MAP/error/NO_SURROUNDED_WALL8.cub
+echo
+./cub3D ./MAP/error/NO_SURROUNDED_WALL9.cub
+echo
+./cub3D ./MAP/error/NO_less.cub
+echo
+./cub3D ./MAP/error/SO_OVER.cub
+echo
+./cub3D ./MAP/error/SO_less.cub
+echo
+./cub3D ./MAP/error/WE_OVER.cub
+echo
+./cub3D ./MAP/error/WE_less.cub
+echo
+./cub3D ./MAP/error/Wrong_position.cub
+echo
+./cub3D ./MAP/error/player_wall_off.cub
+
